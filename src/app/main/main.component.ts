297c721598ef80027a821998eeec91ac8bb53d2a
Dmitriy Gordienko <gordienko@if.ifrigate.ru>
2021-04-26 00:55:02 +0300

    create fast design

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  /**
+   * Статус кнопки
+   */
+  buttonStatus: boolean = false;
+  /**
+   * Флаг загрузки формы
+   */
+  isLoad: any;
+  /**
+   * Тестовые данные для отрисовки
+   */
+  testData = [{
+    text: 'Простейшая задача',
+    order: 1,
+    date: 'today',
+    mark: 'work'
+  }];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  /**
+   * Инициализация компонента
+   */
+  ngOnInit(): void {
+    this.isLoad = true;
+  }
+
+  /**
+   * Добавить задачу
+   */
+  addTask() {
+    this.buttonStatus = true;
+  }
+
+  /**
+   * Отменить добавление
+   */
+  cancelTask() {
+    this.buttonStatus = false;
+  }
+
+  //TODO 1.Для работы с датой использовать momentjs
+  //TODO 2.Для редактирование заметок будет сделан диалог, намного быстрее реализовать редактирование записи, чем пилить динамические формы
+  //TODO 3.Нужно будет повесить гуарды для авторизации и скорее всего посмотреть на JWT реализацию
+  //TODO 4.Авторизация будет на бэке в symfony и редиректить на фронт часть
 }
